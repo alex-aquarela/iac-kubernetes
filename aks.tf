0600c4d1597e30de887bcd1dbc56fea516c132ef
Alex Silva <alex.silva@aquarelacs.com.br>
2023-10-11 14:50:39 -0300

    Atualizando Terraform

--- aks.tf
+++ aks.tf
@@ -1,34 +1,34 @@
-resource "azurerm_resource_group" "rg_grafana" {
-  name     = var.resource_group_grafana
+resource "azurerm_resource_group" "rg_aks_tf" {
+  name     = "rg_aks_tf"
   location = var.resource_group_location
 }
 
-resource "azurerm_storage_account" "asatf_storage_account" {
-  name                     = "stactfadv001prd"
-  resource_group_name      = azurerm_resource_group.rg_grafana.name
-  location                 = azurerm_resource_group.rg_grafana.location
+resource "azurerm_storage_account" "aks_tf_storage_account" {
+  name                     = "aks_tf_storage_account"
+  resource_group_name      = azurerm_resource_group.rg_aks_tf.name
+  location                 = azurerm_resource_group.rg_aks_tf.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_kubernetes_cluster" "aml_aks" {
-  name                = "aks-adv001-prd"
-  location            = azurerm_resource_group.rg_grafana.location
-  resource_group_name = azurerm_resource_group.rg_grafana.name
+resource "azurerm_kubernetes_cluster" "aks_tf" {
+  name                = "aks_tf"
+  location            = azurerm_resource_group.rg_aks_tf.location
+  resource_group_name = azurerm_resource_group.rg_aks_tf.name
   dns_prefix          = "aks"
 
   default_node_pool {
-    name       = "ndadv001prd"
-    node_count = 1
-    vm_size    = "Standard_B2ms"
+    name       = var.default_node_pool_name
+    node_count = var.node_count
+    vm_size    = var.vm_size
   }
   
   identity {
-    type = "SystemAssigned"
+    type = var.aks_identity
   }
 
   network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
+    network_plugin    = var.network_plugin
+    load_balancer_sku = var.load_balancer_sku
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,43 +1,53 @@
-variable "resource_group_grafana" {
-  default = "rg-grafana-adv001-prd"
-}
-
-// g
-variable "grafana_dashboard" {
-  default     = "am-adv001-prd"
-  description = "value"
-}
-
 variable "resource_group_location" {
   default = "eastus2"
 }
 
-/** 
- *  Abaixo estão todas as variáveis de autenticação
- *  
- *  Variável de subscrição
- */
+// Variáveis para autenticação
+
 variable "subscription_id" {
-  default = "3577c57f-b2cc-4e53-9eb5-690756ea2940"
+  type    = string
 }
 
-/** 
- *  Variável de autenticação
- */
 variable "tenant_id" {
-  default = "c49113bf-6e0d-4ed7-b7c0-9d9272dfb531"
+  type    = string
 }
 
-/** 
- *  Variável de autenticação de usuário 
- */
 variable "client_id" {
-  default = "0d8e69bc-8a2f-4ae7-8667-b9d8487d36a5"
+  type    = string
 }
 
-/** 
- *  Variável de autenticação de usuário 
- */
 variable "client_secret" {
-  default = "63b8Q~.iXtREkP19dC3TK_dTtGqLaxN_Z4pmoaxc"
+  type    = string
+}
+
+// Variáveis para configuração do cluster AKS
+
+variable "default_node_pool_name" {
+  type    = string
+  default = "agentpool"
+}
+
+variable "node_count"  {
+  type    = number   
+  default = 1
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2ms"
+}
+
+variable "aks_identity" {
+  type    = string
+  default = "SystemAssigned"
+}
+
+variable "network_plugin" {
+  type    = string
+  default = "kubenet"
+}
+
+variable "load_balancer_sku" {
+  type    = string
+  default = "standard"
 }
